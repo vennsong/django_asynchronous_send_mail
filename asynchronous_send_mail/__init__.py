2f49ac225f3eb03ce76d9f113dbad38c2779570a
vennsong <wenqianggogo@163.com>
2018-05-07 10:20:04 +0800

    Update __init__.py
    add attach_files

--- asynchronous_send_mail/__init__.py
+++ asynchronous_send_mail/__init__.py
@@ -3,22 +3,24 @@ from django.core.mail import EmailMultiAlternatives
 import threading
 
 class EmailThread(threading.Thread):
-    def __init__(self, subject, body, from_email, recipient_list, fail_silently, html):
+    def __init__(self, subject, body, from_email, recipient_list, fail_silently, html, files_list):
         self.subject = subject
         self.body = body
         self.recipient_list = recipient_list
         self.from_email = from_email
         self.fail_silently = fail_silently
         self.html = html
+        self.files_list = files_list
         threading.Thread.__init__(self)
 
     def run (self):
         msg = EmailMultiAlternatives(self.subject, self.body, self.from_email, self.recipient_list)
         if self.html:
             msg.attach_alternative(self.html, "text/html")
+        if self.files_list:
+            for i in self.files_list:
+                msg.attach_file(i)
         msg.send(self.fail_silently)
 
-def send_mail(subject, body, from_email, recipient_list, fail_silently=False, html=None, *args, **kwargs):
-    EmailThread(subject, body, from_email, recipient_list, fail_silently, html).start()
-            
-
+def send_mail(subject, body, from_email, recipient_list, fail_silently=False, html=None, files_list=None, *args, **kwargs):
+    EmailThread(subject, body, from_email, recipient_list, fail_silently, html, files_list).start()
